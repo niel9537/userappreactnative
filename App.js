34ba7f07c4b7722b9a96018ad1add1c9ea31e2d0
LAPTOP-DHA0R3LE\Niel <niel.zebua8@gmail.com>
2022-02-03 21:23:14 +0700

    styling

--- App.js
+++ App.js
@@ -1,112 +1,104 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+import React, {Component} from 'react';
+import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
+import macbook from './mackbookpro.jpg';
+//Functional component App
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+    <Product/>
+    <Product/>
     </View>
-  );
+
+    //<StylingReactNativeComponent/>
+  )
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const Product = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212, borderRadius: 8}}>
+      <Image source={macbook} style={{width: 188, height: 107,borderRadius: 8}}/>
+      <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>Macbook Pro 2020</Text>
+      <Text style={{fontSize: 12, fontWeight: 'bold', marginTop: 14, color: '#F2994A'}}>Rp 17.999.999</Text>
+      <Text style={{fontSize: 12, fontWeight: 'bold', marginTop: 12}}>Jakarta Utara</Text>
+      <View style={{backgroundColor: '#6FCF97', borderRadius: 10, paddingVertical: 6, marginTop: 20}}>
+        <Text style={{fontSize: 14, fontWeight: '300',color: 'white', textAlign: 'center'}}>Beli</Text>
+      </View>
+    </View>
+  )
+}
 
+
+//Component untuk styling
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View 
+      style={{
+        width: 100, 
+        height:100, 
+        backgroundColor: '#0abde3', 
+        borderWidth: 2, 
+        borderColor: '#5f27cd',
+        marginTop: 20,
+        marginHorizontal: 20}}/>
+    </View>
+  )
+}
+//Object style
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  text:{
+    fontSize : 30,
+    fontWeight : 'bold',
+    color: '#10ac84',
+    marginTop: 20,
+    marginLeft: 20
+  }
+})
+//Sample Component
+const SampleComponent = () => {
+  return (
+  <View>
+      <View style={{width:80, height:80, backgroundColor: '#0abde3'}}></View>
+      <Text>Otniel</Text>
+      <Putra/>
+      <ImagePhoto/>
+      <TextInput style={{borderWidth: 1}}></TextInput>
+      <Box/>
+      <Profile/>
+    </View>
+  )
+}
+//Component putra
+const Putra = () => {
+  return <Text>Putra Zebua</Text>
+};
+//Component Image
+const ImagePhoto = () => {
+  return <View>
+  <Image source={{uri:'https://placeimg.com/100/100/animals'}} 
+  style={{height:300,width:250}}/>
+  </View>
+ 
 
+}
+
+//Class component Box
+class Box extends Component{
+  render(){
+    return <Text>Komponen class component</Text>;
+  }
+}
+//Class component profile
+class Profile extends Component{
+  render(){
+    return(
+    <View>
+    <Image  source={{uri:'https://placeimg.com/100/100/animals'}} 
+    style={{height:100,width:100, borderRadius:50}}/>
+    <Text>Aku mencoba</Text>
+    </View> 
+    )
+  }
+}
 export default App;

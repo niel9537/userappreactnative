984df3c4002e26a93b06f32a094315932beabff6
LAPTOP-DHA0R3LE\Niel <niel.zebua8@gmail.com>
2022-02-07 22:04:30 +0700

    finish

--- src/pages/LocalAPI/index.js
+++ src/pages/LocalAPI/index.js
@@ -1,24 +1,26 @@
-import { StyleSheet, Text, TextInput, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Image, TouchableOpacity, Alert } from 'react-native';
 import React, { useEffect } from 'react';
 import mac from '../../assets/image/mackbookpro.jpg'
 import { useState } from 'react';
 import axios from 'axios';
-const Item = ({
-    nama,
-    email,
-    bidang
-}) => {
+
+
+const Item = ({nama,email,bidang,onPress,onDelete}) => {
     return(
         <View>
         <View style={styles.container}>
-          <Image source={mac} style={{height : 100,width:100, borderRadius: 100/2}}/>
+          <TouchableOpacity onPress={onPress}>
+            <Image source={mac} style={{height : 100,width:100, borderRadius: 100/2}}/>
+          </TouchableOpacity>
           <View style={{marginLeft: 10}}>
               <Text>{nama}</Text>
               <Text>{email}</Text>
               <Text>{bidang}</Text>
           </View>
           <View style={{marginLeft: 80}}>
+            <TouchableOpacity onPress={onDelete}>
                 <Text style={{fontSize: 20, fontWeight : 'bold', color: 'red'}}>X</Text>
+            </TouchableOpacity>
           </View>
         </View>
         </View>
@@ -32,7 +34,11 @@ export default function LocalAPI() {
   const [email,setEmail] = useState('');
   const [bidang,setBidang] = useState('');
   const [data,setData] = useState([]);
+  const [button,setButton] = useState('TAMBAH');
+  const [selectedUser,setSelectedUser] = useState('');
   
+
+
   const GetData = () =>{
     axios.get('http://10.0.2.2:3000/users')
     .then(res =>  setData(res.data))
@@ -40,19 +46,53 @@ export default function LocalAPI() {
   }
   
   const Submit = () => {
-    const datad = {
+    const data = {
         nama, 
         email,
         bidang
     }
-    axios.post('http://10.0.2.2:3000/users',datad)
-    .then(res =>  {console.log('Data after sent', res.data)})
-    .catch(error => console.log('Error ',error))
-    GetData();
-
+    if(button === 'SIMPAN'){
+      axios.post('http://10.0.2.2:3000/users',data)
+      .then(res =>  {console.log('Data after sent', res.data)})
+      .catch(error => console.log('Error ',error))
+      GetData();
+    }else if(button === 'UPDATE'){
+      axios.put(`http://10.0.2.2:3000/users/${selectedUser.id}`,data)
+      .then(res =>  {
+        console.log('Data after update', res.data);
+        setNama('');
+        setEmail('');
+        setBidang('');
+        setButton('TAMBAH');
+        GetData();
+      })
+      .catch(error => console.log('Error ',error))
+      
     }
+
+
+  }
+
+  const selectItem = (item) => {
+    console.log('Selected Item : ', item);
+    setNama(item.nama);
+    setEmail(item.email);
+    setBidang(item.bidang);
+    setButton('UPDATE');
+    setSelectedUser(item);
+  }  
+  const deleteItem = (item) => {
+    console.log('Delected Item : ', item);
+    axios.delete(`http://10.0.2.2:3000/users/${item.id}`)
+    .then(res=>console.log('Data after delete :',res))
+    .catch(error => console.log('Error : ',error))
+    GetData();
+  } 
   useEffect(() =>{
     GetData();
+    return(()=>{
+      console.log('Berhasil');
+    })
   },[]);
   return (
     <View style={styles.wrapper}>
@@ -61,8 +101,21 @@ export default function LocalAPI() {
       <TextInput placeholder='Nama Lengkap' style={styles.textStyle} value={nama} onChangeText={(value)=> setNama(value)}></TextInput>
       <TextInput placeholder='Email' style={styles.textStyle} value={email} onChangeText={(value)=> setEmail(value)}></TextInput>
       <TextInput placeholder='Bidang' style={styles.textStyle} value={bidang} onChangeText={(value)=> setBidang(value)}></TextInput>
-      <Button title='Tambah' style={{marginTop : 10}} onPress={()=>Submit()} />
-      {data.map(data => {return <Item key={data.id} nama={data.nama} email={data.email} bidang={data.bidang}/>})}
+      <Button title={button} style={{marginTop : 10}} onPress={()=>Submit()} />
+      {data.map(data => {return <Item 
+      key={data.id} 
+      nama={data.nama} 
+      email={data.email} 
+      bidang={data.bidang} 
+      onPress={() => selectItem(data)} 
+      onDelete={()=> Alert.alert('Peringatan ',
+      'Apakah anda yakin ingin menghapus data akun ini?',
+      [
+        {text:'Tidak',onPress:() =>console.log('Button Tidak')}, 
+        {text: 'Ya', onPress:() =>deleteItem(data)}
+      ])}
+      />})
+      }
     </View>
   );
 }
@@ -85,5 +138,5 @@ const styles = StyleSheet.create({
         flexDirection : 'row',
         alignItems : 'center',
         
-    }
+    } 
 });
